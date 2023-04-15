bf4190dae8134e035a96cb8aff6db81a8ccea53e
toughyear <rajivsingh430@gmail.com>
2023-04-15 17:31:24 +0530

    create a lib for agents and start using it

---
+++ core/typescript/src/agent.ts
@@ -0,0 +1,106 @@
+type AgentPersonality = {
+  background: string;
+  innateTendency: string[];
+  learnedTendency: string[];
+  currentGoal: string;
+  lifestyle: string;
+  values: string[];
+};
+
+type AgentSettings = {
+  visualRange: number;
+  attention: number;
+  retention: number;
+};
+
+type Action = {
+  status: string;
+  emoji: string[];
+};
+
+enum MemoryType {
+  OBSERVATION = "OBSERVATION",
+  REFLECTION = "REFLECTION",
+  PLAN = "PLAN",
+  CONVERSATION = "CONVERSATION",
+}
+
+interface BaseMemory {
+  id: string;
+  createdAt: string;
+  description: string;
+  importance: number;
+  latestAccess: string;
+  embedding: number[];
+}
+
+interface Observation extends BaseMemory {
+  type: MemoryType.OBSERVATION;
+}
+
+interface Reflection extends BaseMemory {
+  type: MemoryType.REFLECTION;
+  evidence: string[];
+}
+
+interface Plan extends BaseMemory {
+  type: MemoryType.PLAN;
+  iteration: number;
+  granularity: "DAY" | "HOUR" | "MINUTE";
+  start: number;
+  end: number;
+  parent: string[];
+}
+
+interface Conversation extends BaseMemory {
+  type: MemoryType.CONVERSATION;
+}
+
+type Memory = Observation | Reflection | Plan | Conversation;
+
+export class Agent {
+  id: string;
+  name: string;
+  age: number;
+
+  personality: AgentPersonality;
+
+  settings: AgentSettings;
+
+  memoryStream: Memory[];
+
+  latestPlanIteration: number;
+
+  action: Action;
+
+  world: object;
+  location: string[];
+
+  constructor(
+    id: string,
+    name: string,
+    age: number,
+    personality: AgentPersonality,
+    settings: AgentSettings = {
+      attention: 8,
+      retention: 8,
+      visualRange: 8,
+    },
+    world: object = {},
+    location: string[] = []
+  ) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.personality = personality;
+    this.settings = settings;
+    this.memoryStream = [];
+    this.latestPlanIteration = 1;
+    this.action = {
+      status: "sleeping",
+      emoji: ["😴"],
+    };
+    this.world = world;
+    this.location = location;
+  }
+}
--- examples/phaser/src/App.tsx
+++ examples/phaser/src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
 import PhaserGame from "./phaserGame";
+import { Agent } from "generative-agents";
 
 function App() {
+  const agent = new Agent("id_1", "Buri Buri Zaemon", 24, {
+    background: "A Samurai",
+    currentGoal: "slay the dragon",
+    innateTendency: ["brave", "rude", "lonewolf"],
+    learnedTendency: ["swordsmanship", "archery", "fishing"],
+    lifestyle: "wanders around the forest of Tsurugi",
+    values: ["honor", "loyalty", "justice"],
+  });
+
+  console.log("agent", agent);
+
   return (
     <div className='p-5 overflow-scroll'>
       <PhaserGame />
