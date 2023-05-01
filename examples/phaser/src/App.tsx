2c79009fc58f620021e412e211b37c17fe81bcad
toughyear <rajivsingh430@gmail.com>
2023-05-01 18:21:48 +0530

    [UI] allow adding your openAI key

--- examples/phaser/src/App.tsx
+++ examples/phaser/src/App.tsx
@@ -1,41 +1,53 @@
-import React, { useRef } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import PhaserGame from "./phaserGame";
-import { Agent, AgentEngine, buildSpatialWorld } from "generative-agents";
-
-import { agentsData } from "./data/agents";
-import AgentDisplay from "./AgentDisplay";
-import { locations } from "./data/world";
+import Game from "./Game";
 
 function App() {
-  const engine = useRef(
-    new AgentEngine(process.env.REACT_APP_OPENAI_API_KEY ?? "")
-  ).current;
+  const openaiKeyFromEnv = process.env.REACT_APP_OPENAI_API_KEY;
 
-  const world = buildSpatialWorld(locations);
+  const [openaiKey, setOpenaiKey] = useState(openaiKeyFromEnv);
+  const [settingKey, setSettingKey] = useState(false);
 
-  const agents: Agent[] = useRef(
-    agentsData.map(
-      (agent) =>
-        new Agent(
-          engine,
-          agent.id,
-          agent.name,
-          agent.age,
-          { ...agent },
-          undefined,
-          world,
-          [agent.startLocation]
-        )
-    )
-  ).current;
+  // first time, if no key, set settingKey to true
+  useEffect(() => {
+    if (!openaiKey) {
+      setSettingKey(true);
+    }
+  }, []);
 
-  return (
-    <div className='p-5 overflow-scroll bg-gray-50'>
-      <AgentDisplay agent={agents[0]} />
-      <PhaserGame agents={agents} />
-    </div>
-  );
+  if (!openaiKey || settingKey) {
+    return (
+      <div className='p-5 overflow-scroll bg-gray-50'>
+        <div className='flex flex-col items-center justify-center h-screen'>
+          <div className='flex flex-col items-center justify-center'>
+            <h1 className='text-3xl font-bold text-gray-900'>
+              Welcome to Generative Agents
+            </h1>
+            <p className='mt-2 text-xl text-gray-600'>
+              Please enter your OpenAI API key to get started.
+            </p>
+          </div>
+          <div className='mt-5'>
+            <input
+              className='px-4 py-2 text-xl border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent'
+              type='text'
+              placeholder='OpenAI API Key'
+              value={openaiKey}
+              onChange={(e) => setOpenaiKey(e.target.value)}
+            />
+            <button
+              className='px-4 py-2 ml-2 text-xl font-bold text-white bg-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent'
+              onClick={() => setSettingKey(false)}
+            >
+              Set Key
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return <Game openaiKey={openaiKey} />;
 }
 
 export default App;
---
+++ examples/phaser/src/Game.tsx
@@ -0,0 +1,44 @@
+import React, { useRef } from "react";
+import PhaserGame from "./phaserGame";
+import { Agent, AgentEngine, buildSpatialWorld } from "generative-agents";
+
+import { agentsData } from "./data/agents";
+import AgentDisplay from "./AgentDisplay";
+import { locations } from "./data/world";
+
+interface Props {
+  openaiKey: string;
+}
+
+function Game(props: Props) {
+  const { openaiKey } = props;
+
+  const engine = useRef(new AgentEngine(openaiKey)).current;
+
+  const world = buildSpatialWorld(locations);
+
+  const agents: Agent[] = useRef(
+    agentsData.map(
+      (agent) =>
+        new Agent(
+          engine,
+          agent.id,
+          agent.name,
+          agent.age,
+          { ...agent },
+          undefined,
+          world,
+          [agent.startLocation]
+        )
+    )
+  ).current;
+
+  return (
+    <div className='p-5 overflow-scroll bg-gray-50'>
+      <AgentDisplay agent={agents[0]} />
+      <PhaserGame agents={agents} />
+    </div>
+  );
+}
+
+export default Game;
